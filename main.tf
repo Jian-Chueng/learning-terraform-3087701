a464383979294972586f17c2868eec2a2e4ab445
Jian Zhang <42600298+Jian-Chueng@users.noreply.github.com>
2024-08-09 00:37:19 +1000

    fix

--- main.tf
+++ main.tf
@@ -21,7 +21,6 @@ data "aws_vpc" "default"{
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
@@ -42,7 +41,7 @@ resource "aws_security_group_rule" "blog_http_in"{
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-aws_security_group_id = aws_security_group.blog.id
+security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in"{
@@ -52,7 +51,7 @@ resource "aws_security_group_rule" "blog_https_in"{
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-aws_security_group_id = aws_security_group.blog.id
+security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out"{
@@ -62,5 +61,5 @@ resource "aws_security_group_rule" "blog_everything_out"{
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-aws_security_group_id = aws_security_group.blog.id
+security_group_id = aws_security_group.blog.id
 }
