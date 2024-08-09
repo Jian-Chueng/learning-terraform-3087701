70b4b5e1987cbe5af761d7a378a77e6598a80d6d
Jian Zhang <42600298+Jian-Chueng@users.noreply.github.com>
2024-08-09 23:45:12 +1000

    load balancer

--- main.tf
+++ main.tf
@@ -25,7 +25,6 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
-
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -46,6 +45,50 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "Blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = [
+        {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+
+      ]
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
 
 
 module "blog_sg" {
